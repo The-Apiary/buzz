501fad625a4a98626fce314733877a339636ecc5
Caleb Everett <everett.caleb@gmail.com>
2014-01-04 01:31:25 -0500

    Fixed some dumb bugs that shouldn't have been commited

--- app/controllers/ember_controller.rb
+++ app/controllers/ember_controller.rb
@@ -1,11 +1,11 @@
 class EmberController < ApplicationController
   def player
-    @user ||= User.where(id_hash: params[:id_hash])
-    login(user)
+    @user ||= User.find_by_id_hash(params[:id_hash])
+    login(@user)
   end
 
   def start
     @user = User.create
-    redirect_to player_path(user, id_hash: user.id_hash)
+    redirect_to player_path(@user, id_hash: @user.id_hash)
   end
 end
