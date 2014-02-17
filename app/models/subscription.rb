102333706a0be77265a75e7c0276039bce2e8bbd
Caleb Everett <everett.caleb@gmail.com>
2014-02-17 18:10:37 -0500

    Sort subscriptions by podcast title

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,6 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
 
   validates :podcast, uniqueness: { scope: :user}
+
+  default_scope { includes(:podcast).order('podcasts.title') }
 end
