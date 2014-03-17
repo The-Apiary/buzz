5e55d94f5f3d8c5637b3920f3c06246f2a5aedf8
Caleb Everett <everett.caleb@gmail.com>
2014-03-17 17:22:29 -0400

    Add subscription type to the database

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,24 @@
 class Subscription < ActiveRecord::Base
+  #-- Associations
   belongs_to :podcast
   belongs_to :user
 
+  #-- callbacks
+  before_validation :downcase_subscription_type
+
+  #-- Validattions
   validates :podcast, uniqueness: { scope: :user}
 
+  # Valid podcast subscriptions types
+  valid_types = ['normal', 'serial', 'news']
+  validates :subscription_type, inclusion: {in: valid_types, message: "'%{value}' is not in #{valid_types.inspect}"}
+
+  #-- Scopes
   default_scope { includes(:podcast).order('podcasts.title') }
+
+  private
+
+  def downcase_subscription_type
+    self.subscription_type.downcase!
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315182823) do
+ActiveRecord::Schema.define(version: 20140317195519) do
 
   create_table "episode_data", force: true do |t|
     t.integer "episode_id"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20140315182823) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "subscription_type", default: "normal", null: false
   end
 
   add_index "subscriptions", ["podcast_id"], name: "index_subscriptions_on_podcast_id"
