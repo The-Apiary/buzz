4b118c8021782de31edfc9aefb47ede4194fd3c1
Caleb Everett <everett.caleb@gmail.com>
2014-04-05 22:31:17 -0400

    Finish QueueManager object

--- app/models/queue_manager.rb
+++ app/models/queue_manager.rb
@@ -5,45 +5,133 @@ class QueueManager
     end
 
     @user = user
+    @was_rebased = false
+  end
+
+  def rebased?
+    @was_rebased
   end
 
   def clear
     @user.queued_episodes.destroy_all
   end
 
-  def enqueue(episode)
-    @user.queued_episodes.where(episode: episode).first_or_create(idx: tail_index)
+  def push(episode)
+    begin
+      move_to_index episode, tail_index
+    rescue GapTooSmall
+      rebase
+      push episode
+    end
   end
 
-  def head_enqueue(episode)
-    @user.queued_episodes.where(episode: episode).first_or_create(idx: head_index)
+  def shift(episode)
+    begin
+      move_to_index episode, head_index
+    rescue GapTooSmall
+      rebase
+      shift episode
+    end
   end
 
-  def dequeue(episode)
+  def add_between(episode, after: after, before: before)
+    begin
+      idxs = [episode_index(after), episode_index(before)]
+
+      if idxs.any?(&:nil?)
+        raise ArgumentError.new "before and after must be queued"
+      end
+
+      move_to_index(episode, index(*idxs))
+    rescue GapTooSmall
+      rebase
+      add_between(episode, after: after, before: before)
+    end
+  end
+
+  def remove(episode)
     @user.queued_episodes.where(episode: episode).first.tap do |e|
       e.destroy
     end
   end
 
-
   # Min value of a Postgresql Integer
-  def min_idx
+  def self.min_idx
     -2147483648
   end
 
   # Max value of a Postgresql Integer
-  def max_idx
+  def self.max_idx
     2147483647
   end
-   
+
   private
 
+  ##
+  # Evenly distributes the queued episodes throughout the address space
+  def rebase
+    Rails.logger.tagged(:queue, :rebase) { Rails.logger.info "rebasing user #{@user.id}'s queue" }
+    @was_rebased = true
+    qes = @user.queued_episodes
+    range = (QueueManager.min_idx..QueueManager.max_idx)
+
+    # if the address space is 10 percent full don't rebase, just throw an
+    # error.
+    #
+    # What is someone doing with 429496729 episodes in the queue?
+    if qes.size > (range.size * 0.20)
+      raise QueueTooLarge.new "Wut"
+    end
+
+    # Add two to the size for beginning and end padding
+    # and add another for fence posts
+    step = (range.size / (qes.size + 1))
+
+    if qes.size == 1
+      # there are some rounding errors when size is one
+      indexes = [0].each
+    else
+      indexes = ((QueueManager.min_idx+step)..(QueueManager.max_idx-step)).step(step).each
+    end
+
+    qes.each do |qe|
+      qe.idx = indexes.next
+      qe.save
+    end
+
+    @user.queued_episodes.reload
+
+    Rails.logger.tagged(:queue, :rebase) { Rails.logger.info "done rebasing user #{@user.id}'s queue" }
+
+  end
+
+  def episode_index episode
+    if episode.is_a? Episode
+      @user.queued_episodes.find_by_episode_id(episode).try(:idx)
+    else
+      raise ArgumentError "episodes must be owned by user"
+    end
+
+  end
+
+  ##
+  # Set the index of the queued episode with episode e
+  # to half way between f, and s
+  def move_to_index e, idx
+    raise ArgumentError "e must be an episode" unless e.is_a? Episode
+
+    qe = @user.queued_episodes.where(episode: e).first_or_initialize
+    qe.update_attributes(idx: idx)
+
+    return qe
+  end
+
   def tail_index
-    index(@user.queued_episodes.last.try(:idx) || min_idx, max_idx)
+    index(@user.queued_episodes.last.try(:idx) || QueueManager.min_idx, QueueManager.max_idx)
   end
 
   def head_index
-    index(min_idx, @user.queued_episodes.first.try(:idx) || max_idx)
+    index(QueueManager.min_idx, @user.queued_episodes.first.try(:idx) || QueueManager.max_idx)
   end
 
   def index(low, high)
@@ -59,4 +147,10 @@ class QueueManager
       return idx
     end
   end
+end
+
+class GapTooSmall < Exception
+end
+
+class QueueTooLarge < Exception
 end
--- test/models/queue_manager_test.rb
+++ test/models/queue_manager_test.rb
@@ -19,8 +19,18 @@ class QueueManagerTest < ActiveSupport::TestCase
     assert_equal "Argument false must be User but was FalseClass", err.message
   end
 
+  #-- Min/Max
+
+  test "Should have a max index of 2147483647" do
+    assert_equal 2147483647, QueueManager.max_idx
+  end
+
+  test "Should have a min index of 2147483648" do
+    assert_equal (-2147483648), QueueManager.min_idx
+  end
 
   #-- Clear
+
    test "#clear: Should delete all queued episodes" do
     user = create(:user)
     qm = QueueManager.new(user)
@@ -34,92 +44,219 @@ class QueueManagerTest < ActiveSupport::TestCase
     assert_equal 0, user.queued_episodes.count
    end
 
-  #-- Queueing
+  #-- Push
 
-  test "enqueue: Should create queued episodes" do
+  test "push: Should create queued episodes" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
 
     assert_difference 'QueuedEpisode.count', 1 do
-      qm.enqueue(episode)
+      qm.push(episode)
     end
   end
 
-  test "enqueue: Should not create duplicate queued episodes" do
+  test "push: Should not create duplicate queued episodes" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
 
-    qm.enqueue(episode)
+    qm.push(episode)
     assert_no_difference 'QueuedEpisode.count' do
-      qm.enqueue(episode)
+      qm.push(episode)
     end
   end
 
-  test "enqueue: Should add episodes to the end of the queue" do
+  test "push: Should add episodes to the end of the queue" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
-    qm.enqueue(create(:episode))
+    qm.push(create(:episode))
 
-    qe = qm.enqueue(episode)
+    qe = qm.push(episode)
     assert_equal user.queued_episodes.last, qe
     assert_equal 2, user.queued_episodes.count
   end
 
-  #-- Head queue
+  test "push: Should move episode if already exists" do
+    user = create(:user)
+    episode = create(:episode)
 
-  test "head_enqueue: Should create queued episodes" do
+    qm = QueueManager.new(user)
+
+    qm.push(episode)
+    qm.push(create(:episode))
+    assert_no_difference 'QueuedEpisode.count' do
+      qe = qm.push(episode)
+      assert_equal user.queued_episodes.last, qe
+    end
+  end
+
+  #-- Shift
+
+  test "shift: Should create queued episodes" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
 
     assert_difference 'QueuedEpisode.count', 1 do
-      qm.head_enqueue(episode)
+      qm.shift(episode)
     end
   end
 
-  test "head_enqueue: Should not create duplicate queued episodes" do
+  test "shift: Should not create duplicate queued episodes" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
 
-    qm.enqueue(episode)
+    qm.push(episode)
     assert_no_difference 'QueuedEpisode.count' do
-      qm.head_enqueue(episode)
+      qm.shift(episode)
     end
   end
 
-  test "head_enqueue: Should add episodes to the beginning of the queue" do
+  test "shift: Should add episodes to the beginning of the queue" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
-    qm.enqueue(create(:episode))
+    qm.push(create(:episode))
 
-    qe = qm.head_enqueue(episode)
+    qe = qm.shift(episode)
     assert_equal user.queued_episodes.first, qe
     assert_equal 2, user.queued_episodes.count
   end
 
-
-  #-- Dequeue
-
-  test "dequeue: Should delete queued episodes" do
+  test "push: Should move episode to front if already exists" do
     user = create(:user)
     episode = create(:episode)
 
     qm = QueueManager.new(user)
-    qm.enqueue(episode)
 
-    assert_difference 'QueuedEpisode.count', -1 do
-      qm.dequeue(episode)
+    qm.push(create(:episode))
+    qm.push(episode)
+    assert_no_difference 'QueuedEpisode.count' do
+      qe = qm.shift(episode)
+      assert_equal user.queued_episodes.first, qe
     end
   end
+
+  #-- Add between
+
+  test "add_between: Should create queeud episode" do
+    user = create(:user)
+    episode = create(:episode)
+
+    qm = QueueManager.new(user)
+
+    e1 = create(:episode)
+    e2 = create(:episode)
+
+    qm.push e1
+    qm.push e2
+    assert_difference 'QueuedEpisode.count', 1 do
+      qe = qm.add_between(episode, after: e1, before: e2)
+      assert_equal episode, qe.episode
+    end
+  end
+
+  test "add_between: Should add queeud episode between others" do
+    user = create(:user)
+    episode = create(:episode)
+
+    qm = QueueManager.new(user)
+
+    e1 = create(:episode)
+    e2 = create(:episode)
+
+    qm.push e1
+    qm.push e2
+    assert_difference 'QueuedEpisode.count', 1 do
+      qm.add_between(episode, after: e1, before: e2)
+
+      assert_equal user.queued_episodes.first.episode, e1
+      assert_equal user.queued_episodes.last.episode, e2
+    end
+  end
+
+  #-- Remove
+
+  test "Remove: Should delete queued episodes" do
+    user = create(:user)
+    episode = create(:episode)
+
+    qm = QueueManager.new(user)
+    qm.push(episode)
+
+    assert_difference 'QueuedEpisode.count', -1 do
+      qm.remove(episode)
+    end
+  end
+
+  #-- Rebase test
+
+  test "Should rebase when gap gets too small" do
+    user = create(:user)
+    qm = QueueManager.new(user)
+
+    e1 = create(:episode)
+    e2 = create(:episode)
+    user.queued_episodes.create(episode: e1, idx: 0)
+    user.queued_episodes.create(episode: e2, idx: 1)
+
+    assert_difference 'user.queued_episodes.count' do
+      qm.add_between(create(:episode), after: e1, before: e2)
+      assert_equal true, qm.rebased?
+    end
+  end
+
+  #test "Should rebase when tail gets too crowded" do
+  #  user = create(:user)
+  #  qm = QueueManager.new(user)
+
+  #  e = create(:episode)
+  #  user.queued_episodes.create(episode: e, idx: QueueManager.max_idx)
+
+  #  assert_difference 'user.queued_episodes.count', 1 do
+  #    qm.push(create(:episode))
+  #    assert_equal true, qm.rebased?
+  #  end
+  #end
+
+  test "Should rebase when head gets too crowded" do
+    user = create(:user)
+    qm = QueueManager.new(user)
+
+    e = create(:episode)
+    user.queued_episodes.create(episode: e, idx: QueueManager.min_idx)
+
+    assert_difference 'user.queued_episodes.count', 1 do
+      qm.shift(create(:episode))
+      assert_equal true, qm.rebased?
+    end
+  end
+
+  test "Should be able to push 32 episodes without rebasing" do
+    user = create(:user)
+    qm = QueueManager.new(user)
+
+    order = 32.times.map { qm.push(create(:episode))}
+    assert_equal false, qm.rebased?
+    assert_equal order, user.queued_episodes
+
+    assert_difference 'user.queued_episodes.count', 1 do
+      order << qm.push(create(:episode))
+      assert_equal true, qm.rebased?
+
+      user.reload
+      assert_equal order.count, user.queued_episodes.count
+      assert_equal order, user.queued_episodes
+    end
+  end
+
 end
