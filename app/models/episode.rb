35a11ca24978e40254ca0bdb1409a860cec2819b
Caleb Everett <everett.caleb@gmail.com>
2014-04-29 14:58:38 -0400

    Add last listened time to episode

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -67,4 +67,8 @@ class Episode < ActiveRecord::Base
   def is_played(user)
     episode_data(user).try(:is_played) || false
   end
+
+  def is_played(user)
+    episode_data(user).try(:updated_at)
+  end
 end
